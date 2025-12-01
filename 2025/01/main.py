9a409e851df236e6ec0607cc40956d1a88a7734f
Matthew Morgis <matthew.morgis@gmail.com>
2025-12-01 12:55:20 -0500

    2025 day 1 part 2

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Literal, Tuple
 
 
 class Rotation:
@@ -18,43 +18,55 @@ def parse_input() -> list[Rotation]:
     return rotations
 
 
-def part_one(rotations: list[Rotation]) -> int:
+def day_one(rotations: list[Rotation]) -> Tuple[int, int]:
     start = 0
     current = 50
     end = 100
-    times_at_start = 0
+    ends_on_zero = 0
+    passes_zero = 0
 
     for rotation in rotations:
         print(
             f"Rotation.direction: {rotation.direction} \t Rotation.distance: {rotation.distance}"
         )
         if rotation.distance > end:
+            passes_zero = passes_zero + int(rotation.distance / end)
             rotation.distance = rotation.distance % end
 
+        landed_on_zero = current == 0
         if rotation.direction == "R":
             current = current + rotation.distance
             if current == end:
                 current = start
             elif current > end:
+                if not landed_on_zero:
+                    passes_zero = passes_zero + 1
+                    print(f"**passes zero R: {passes_zero}")
                 current = abs(end - current)
         else:
             current = current - rotation.distance
             if current < start:
+                if not landed_on_zero:
+                    passes_zero = passes_zero + 1
+                    print(f"**passes zero L: {passes_zero}")
                 current = end + current
 
         print(f"current dial: {current}")
 
         if current == start:
-            times_at_start = times_at_start + 1
+            ends_on_zero = ends_on_zero + 1
+            passes_zero = passes_zero + 1
+            print(f"**lands on zero: {passes_zero}")
 
-    return times_at_start
+    return (ends_on_zero, passes_zero)
 
 
 def main():
     rotations = parse_input()
-    part_one_answer = part_one(rotations)
+    part_one, part_two = day_one(rotations)
     print()
-    print(f"Part One Answer: {part_one_answer}")
+    print(f"Part One Answer: {part_one}")
+    print(f"Part Two Answer: {part_two}")
 
 
 main()
